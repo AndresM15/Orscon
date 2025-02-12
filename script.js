8c4f019c97030261aaa87d7fbd1e1d4efe3f6b6f
AndresMon <andres.montoyao@autonoma.edu.co>
2025-02-12 17:51:24 -0500

    Se creo la estructura de la pagina web

---
+++ script.js
@@ -0,0 +1,13 @@
+let indiceActual = 0;
+const imagenes = document.querySelectorAll('.imagen');
+
+function cambiarImagen(direccion) {
+    
+    imagenes[indiceActual].classList.remove('activa');
+    
+
+    indiceActual = (indiceActual + direccion + imagenes.length) % imagenes.length;
+    
+
+    imagenes[indiceActual].classList.add('activa');
+}
